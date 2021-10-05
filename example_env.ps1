6750ac26837ecbe91e57af686d4d5d6e86412377
Doug Renze <drenze@athene.com>
2021-10-05 11:29:19 -0500

    update service name

--- example_env.ps1
+++ example_env.ps1
@@ -12,5 +12,5 @@ $env:ELASTIC_APM_ENVIRONMENT = "demo"  # Usually one of: (dev|qa|stage|prod)
 $env:ELASTIC_APM_LOG_LEVEL = "info"  # One of: (off|critical|error|warning|info|debug|trace)
 $env:ELASTIC_APM_RECORDING = "true"  # (true|false)
 $env:ELASTIC_APM_SERVER_URL = "https://elk.onintranet.com:8200"
-$env:ELASTIC_APM_SERVICE_NAME = "demo-review-service-$($($(whoami) -Split '\\')[-1])"
+$env:ELASTIC_APM_SERVICE_NAME = "review-service-$($($(whoami) -Split '\\')[-1])"
 $env:ELASTIC_APM_VERIFY_SERVER_CERT = "False"  # (True|False)
